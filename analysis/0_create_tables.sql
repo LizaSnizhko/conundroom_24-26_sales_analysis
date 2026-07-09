c7408c41e4496b81dc98373aa81d25f014877294
Liza <snizhko.liza@gmail.com>
2026-07-08 23:42:53 -0700

    add new tables, new features and updated columns

--- analysis/0_create_tables.sql
+++ analysis/0_create_tables.sql
@@ -1,3 +1,5 @@
+----1---- Table for online bookings from Bookeo.com from July 1st to June 30th 2026
+
 CREATE TABLE bookings (
     booking_number          TEXT PRIMARY KEY,
     start_time              TIMESTAMP,
@@ -30,7 +32,7 @@ CREATE TABLE bookings (
 
 SELECT * FROM bookings
 
---\copy customers FROM '/Users/lizasnizhko/Library/CloudStorage/OneDrive-Personal/projects/conundroom_analysis/conundroom_analysis_sql_project/data/combined_customers.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+-----2---- Table for customers for all times ever
 
 CREATE TABLE customers (
     last_name         TEXT,
@@ -59,6 +61,10 @@ SELECT DISTINCT escape_room
 FROM bookings
 ORDER BY escape_room
 
+
+
+------3------ Table to map escape room names to categories (e.g. single escape room, special events, etc.)
+
 CREATE TABLE booking_categories (
     escape_room TEXT,
     booking_category TEXT
@@ -68,3 +74,69 @@ SELECT *
 FROM booking_categories
 
 --\copy booking_categories FROM '/Users/lizasnizhko/Library/CloudStorage/OneDrive-Personal/projects/conundroom_analysis/conundroom_analysis_sql_project/data/booking_categories.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+ALTER TABLE booking_categories
+ADD COLUMN on_site_sales_corresponding_item TEXT;
+
+UPDATE booking_categories
+SET on_site_sales_corresponding_item =
+CASE
+    WHEN escape_room = 'Alice in Wonderland' THEN 'ADD Alice player'
+    WHEN escape_room = 'Crafted' THEN 'ADD Crafted player'
+    WHEN escape_room = 'Cursed.' THEN 'ADD Cursed player'
+    WHEN escape_room = 'Dr. Frankenstein' THEN 'ADD Frankenstein player'
+    WHEN escape_room = 'Luck and Key Bar' THEN 'ADD Luck & Key Bar player'
+    WHEN escape_room = 'School of Magic' THEN 'ADD School of Magic player'
+    WHEN escape_room = 'Tesla''s Laboratory' THEN 'ADD Tesla Player'
+    WHEN escape_room = 'The Ghost Ship' THEN 'ADD Ghost Ship player'
+    WHEN escape_room = 'The Northwest Express' THEN 'ADD NW Express player'
+    WHEN escape_room = 'The Vault' THEN 'ADD The Vault player'
+    WHEN escape_room = 'The Wizard''s Chest' THEN 'ADD Wizard''s Chest player'
+    WHEN escape_room = 'Zeppelin' THEN 'ADD Zeppelin player'
+    WHEN escape_room = 'Red vs. Blue: BOMB Defuse Challenge' THEN 'Special Event'
+    ELSE 'Unsorted'
+END;
+
+
+INSERT INTO booking_categories (escape_room, booking_category, on_site_sales_corresponding_item)
+VALUES ('The Vault', 'Escape Room', 'ADD Time Travel player');
+
+SELECT * FROM booking_categories
+
+-------4------- Table for on-site sales from the Square payment system from July 1st to June 30th 2026
+
+CREATE TABLE on_site_sales (
+    date TIMESTAMP,
+    category TEXT,
+    item TEXT,
+    count INTEGER,
+    gross_sales NUMERIC(10,2),
+    discounts NUMERIC(10,2),
+    net_sales NUMERIC(10,2),
+    tax NUMERIC(10,2),
+    status TEXT,
+    transaction_id TEXT,
+    payment_id TEXT,
+    device_id TEXT,
+    notes TEXT,
+    details TEXT,
+    customer_id TEXT,
+    customer_name TEXT
+)
+
+SELECT * FROM on_site_sales
+
+-- \copy on_site_sales FROM '/Users/lizasnizhko/Library/CloudStorage/OneDrive-Personal/projects/conundroom_analysis/conundroom_analysis_sql_project/data/on_site_payments.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+------5------ Table with information about each escape room active period, as not each escape room was active or open for the entire 2 years the analysis is done for
+
+CREATE TABLE escape_room_active_periods
+(
+    escape_room TEXT,
+    start_date DATE,
+    end_date DATE
+)
+
+-- \copy escape_room_active_periods FROM '/Users/lizasnizhko/Library/CloudStorage/OneDrive-Personal/projects/conundroom_analysis/conundroom_analysis_sql_project/data/escape_room_active_periods.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+SELECT * FROM  escape_room_active_periods;
