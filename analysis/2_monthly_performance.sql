51f730eceb48c7f703b6c0177c18d1b5b9809b6b
Liza <snizhko.liza@gmail.com>
2026-07-17 01:48:05 -0700

    update canceled bookings filtering and updated the room_performance view

--- analysis/2_monthly_performance.sql
+++ analysis/2_monthly_performance.sql
@@ -3,16 +3,21 @@
 CREATE OR REPLACE VIEW monthly_performance AS
     WITH on_site_sales_summary AS (
         SELECT 
-            DATE_TRUNC('month',date) as month,
-            SUM(on_site_sales.corrected_total_net) AS total_net
+            DATE_TRUNC('month', date) as month,
+            SUM(corrected_total_net) AS total_net,
+            SUM(count) FILTER 
+                (WHERE item in (SELECT on_site_sales_corresponding_item 
+                FROM booking_categories 
+                WHERE on_site_sales_corresponding_item <> 'Unsorted')) AS total_players
         FROM on_site_sales  
-        GROUP BY DATE_TRUNC('month',date)
+        GROUP BY DATE_TRUNC('month', date)
     ),
     bookings_summary AS (
         SELECT 
             DATE_TRUNC('month',start_time) as month,
             SUM(corrected_total_net) AS total_net,
-            COUNT(*) FILTER(WHERE NOT (status = 'canceled' AND corrected_total_net <= 0)) AS total_bookings
+            SUM(participants) FILTER(WHERE status <> 'canceled') AS total_players,
+            COUNT(*) FILTER(WHERE status <> 'canceled') AS total_bookings
         FROM bookings
         GROUP BY DATE_TRUNC('month',start_time)
     )
@@ -20,11 +25,10 @@ CREATE OR REPLACE VIEW monthly_performance AS
     SELECT 
         bookings_summary.month,
         bookings_summary.total_net + COALESCE(on_site_sales_summary.total_net,0) AS total_revenue,
-        bookings_summary.total_bookings
+        bookings_summary.total_bookings,
+        bookings_summary.total_players + on_site_sales_summary.total_players AS total_players
     FROM bookings_summary 
     LEFT JOIN on_site_sales_summary USING(month)
     ORDER BY month;
-
-
 
 SELECT * FROM monthly_performance
